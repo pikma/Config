e9a04d0b27e813c585193d68f29a5797f7a79ca9
Pierre Kreitmann <kreitmann@google.com>
2014-09-08 09:15:09 +0200

    Fix symlinks

--- set_up_symlinks.sh
+++ set_up_symlinks.sh
@@ -1,7 +1,7 @@
 files="bashrc gitconfig inputrc tmux.conf vim vimrc"
 cd $HOME
 for f in $files; do
-  ln -s "~/.myConfig/$f" ".$f"
+  ln -s ".myConfig/$f" ".$f"
   echo "Created symlink .$f"
 done
 
