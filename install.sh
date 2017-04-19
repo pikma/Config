8b7ada78c3bd4c110b1e555addb5c6d7fe1181ed
Pierre Kreitmann <kreitmann@google.com>
2017-04-19 15:17:22 -0700

    Install gnome-terminal colors by default

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+echo $(dirname $0)
+
+exit
+
 sudo apt-get update && \
 sudo apt-get install \
   clang-3.5 \
@@ -21,3 +25,6 @@ echo "\n\n===================================="
 echo "Need to manually install:"
 echo "  Google Chrome: http://google.com/chrome"
 echo "  Anaconda (Jupyter Notebook): https://www.continuum.io/downloads"
+
+# Load the gnome-terminal settings:
+gconftool-2 --load "$(dirname "$0")/gnome_terminal_conf.xml"
