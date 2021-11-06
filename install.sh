f10a8fd20738a9c78abf9eda6dc4d203c9100602
Pierre Kreitmann <kreitmann@google.com>
2021-11-05 21:19:06 -0700

    YCM installation instructions

--- install.sh
+++ install.sh
@@ -16,3 +16,10 @@ echo "  Google Chrome: http://google.com/chrome"
 # Load the gnome-terminal settings:
 echo "To load the gnome-terminal settings, run:"
 echo "gconftool-2 --load \"$(dirname "$0")/gnome_terminal_conf.xml\""
+
+echo "\nTo install YouCompleteMe, check out https://github.com/ycm-core/YouCompleteMe#installation."
+echo "As of 2021-11-05, the commands to run were:"
+echo "  apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm"
+echo "  cd ~/.vim/bundle/YouCompleteMe && python3 install.py --all"
+
+
